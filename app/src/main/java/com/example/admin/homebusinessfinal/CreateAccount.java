a430cc289033bf3fab7afc5fd98e0cf32fe2584b
Kaustubh <shahkaustubh3@gmail.com>
2019-07-31 13:07:06 +0530

    Added Firebase Support and create account page

--- app/src/main/java/com/example/admin/homebusinessfinal/CreateAccount.java
+++ app/src/main/java/com/example/admin/homebusinessfinal/CreateAccount.java
@@ -1,28 +1,21 @@
 package com.example.admin.homebusinessfinal;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
-public class Login extends AppCompatActivity {
+public class CreateAccount extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_create_account);
 
-        //set up Toolbar and add up button
-        Toolbar toolbar = findViewById(R.id.toolbar_login);
+        //set up toolbar and up button
+        Toolbar toolbar = findViewById(R.id.toolbar_createAccount);
         setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
-    }
-
-    public void openCreateAccount(View view){
-        Intent intent = new Intent(Login.this,CreateAccount.class);
-        startActivity(intent);
     }
 }
